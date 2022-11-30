7d5dbc501f3497615bae03b9434ca96e8d197963
Lucas Boistay <lucasboistay@hotmail.fr>
2022-11-30 23:21:05 +0100

    Ajout test getteur list de Surface dans Test

--- src/Test.java
+++ src/Test.java
@@ -9,12 +9,12 @@ public class Test {
 
         System.out.println("Test des points :");
 
-        Point p = new Point(0, 0);
+        Point p1 = new Point(0, 0);
         Point p2 = new Point(3, 0);
 
         System.out.println("On affiche le point P2 " + p2);
 
-        System.out.println("Distance entre p et P2 " + p.distance(p2));
+        System.out.println("Distance entre p et P2 " + p1.distance(p2));
 
         System.out.println("Test des surfaces");
 
@@ -24,12 +24,16 @@ public class Test {
 
         System.out.println("Affichage surface 1 : " + s);
 
-        s.ajoutePoint(p);
+        s.ajoutePoint(p1);
         s.ajoutePoint(p2);
         s.ajoutePoint(p3);
 
+        System.out.println("Affichage des Points contenu dans la surface :");
+
+        for (Point p : s.getList()){
+            System.out.println(p);
+        }
+
         System.out.println("Affichage surface 1 : " + s);
-
-
     }
 }    
